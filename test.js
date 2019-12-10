6febcc5e9b82d599fd1572737876e64e7515d52e
Adam Rafiandri <rafiandria23@gmail.com>
2019-12-10 16:56:53 +0700

    Created cari-pelaku.js

---
+++ test.js
@@ -0,0 +1,3 @@
+var string = 'Walaupun regex banyak mengandung simbol, tapi tidak serumit seperti !@#%^%#$*( , ^%&*!!^& dan !#*#$&*@%#';
+console.log(string.match(/[a-z]+/gi));
+//menampilkan ["Walaupun", "regex", "banyak", "mengandung", "simbol", "tapi", "tidak", "serumit", "seperti", "dan"]
